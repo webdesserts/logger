a0b8e0e03576784d698a23d690335c1b2c315492
Michael Mullins <michael@webdesserts.com>
2019-08-20 18:11:12 -0500

    center App on larger screens

--- src/App.styles.tsx
+++ src/App.styles.tsx
@@ -5,6 +5,7 @@ export const App = styled.div`
   grid-auto-flow: column;
   grid-template-columns: minmax(50vw, 670px) 360px;
   justify-items: stretch;
+  justify-content: center;
   padding: 64px;
   grid-gap: 24px;
   min-height: 100%;
